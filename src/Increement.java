b7354b275154939597fa4d3600cf9b231a2b3e4a
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-02-25 12:48:42 -0600

    increement

---
+++ src/Increement.java
@@ -0,0 +1,9 @@
+public class Increement {
+    public static void main(String[] args) {
+        //Arithmetic
+        int sum = 2;
+        int inc = sum++;
+        System.out.println(inc);//this returns the operation value
+        System.out.println(sum);//this returns the operation output
+    }
+}
