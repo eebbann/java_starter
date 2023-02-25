605da903cf194beb520bdc8249cea4e6f9decb05
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-02-25 12:25:41 -0600

    starting basic with int

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int sum = 100_000;
+        System.out.println(40 * sum);
+    }
+}
