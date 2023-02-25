76a40a1b64a9742a60624bc09b2dfd54542f69bd
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-02-25 12:29:16 -0600

    starting basic with int

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Arithmetic
         int sum = 100_000;
-        System.out.println(40 * sum);
+        System.out.println(sum/2);
+        System.out.println(sum % 300);
+        System.out.println(sum + 3);
+        System.out.println(sum * 7);
+        System.out.println(40 + sum);
     }
 }
