3e2b1106e4046786d60dc1db7692745fb5a96805
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-02-25 14:01:51 -0600

    decreement

---
+++ src/Decreement.java
@@ -0,0 +1,11 @@
+public class Decreement {
+        public static void main(String[] args) {
+            //understanding Arithmetic (revision)
+            int sum = 4;
+            int inc = sum--;
+            System.out.println(inc);//this returns the operation value
+            System.out.println(sum);//this returns the operation output
+        }
+    }
+
+
