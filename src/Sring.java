fdac625f71138ec4cf38ddf63fe59aa84c5da159
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-03-10 13:36:51 -0600

    understanding scanner utility with int

---
+++ src/Sring.java
@@ -0,0 +1,36 @@
+public class Sring {
+    public static void main(String[] helloArray) {
+
+//        char[] h = {'h', 'e', 'l', 'l', 'o', '.'};
+//        String helloString = new String (h); //converted the char into string
+//        System.out.println(helloString);
+//        // methods charAt
+//        String myStr = "Hello";
+//        char result = myStr.charAt(0);
+//        System.out.println(result);
+//
+//        //method codePointAt
+//        String yStr = "Hello";
+//        int sult = yStr.codePointAt(0);
+//        System.out.println(sult);
+//
+//        // concatenating two stings
+//        String man = "open";
+//        String woman = "close";
+//        System.out.println(man.concat(woman));
+//
+//        // method contains
+//        System.out.println(man.contains("e"));
+//
+//        //method indexOf
+//        System.out.println(yStr.indexOf("H"));
+//        //method replace
+//        System.out.println(yStr.replace('l', 'p'));
+
+          System.out.println((3 + 4) * (5 / 2));
+          System.out.println((-5) * (-10) * (-20));
+          System.out.println(10 / (2 + 2) + 4 * 8);
+          System.out.println(2 + (-3) + 5 + (-8));
+
+    }
+}
